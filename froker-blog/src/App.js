5d1db185b041c3d79b566de5e6005f23357b32a3
Sanil Singh <sanilsingh3008@gmail.com>
2024-07-20 00:30:09 +0530

    first-commit-0030

---
+++ froker-blog/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import BlogHeader from './components/BlogHeader';
+import BlogContent from './components/BlogContent';
+
+function App() {
+  return (
+    <div className="App">
+      <BlogHeader />
+      <BlogContent />
+    </div>
+  );
+}
+
+export default App;
